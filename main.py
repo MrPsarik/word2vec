8800ebf9618c79f5cf8f1bbeb45e0c2de2501556
MrPsarik <103962353+MrPsarik@users.noreply.github.com>
2026-03-19 13:52:01 +0100

    refactor(main): moved text preprocessing functions to word2vec

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-
-import numpy as np
+from word2vec import Word2Vec
 
 
 FILE_PATH = "data\\AllCombined_half.txt"
@@ -13,33 +11,10 @@ def text_load(file_path, size=-1) -> str:
         return text 
 
 
-def text_cleanup(text) -> list[str]:
-    # Delete all non-letter characters 
-    text = re.sub(r"[^a-zA-Z\s+]", "", text)
-
-    # Convert all characters to lowercase
-    text = text.casefold()
-
-    # Split text into words
-    text = text.split()
-
-    return text
-
-
-def text_to_dict(text) -> dict[str, int]:
-    dict = {}
-    text = set(text)
-
-    for i, word in enumerate(text):
-        dict[word] = i
-
-    return dict
-
-
 if __name__ == "__main__": 
 
-    text = text_load(FILE_PATH, TEXT_SIZE)
+    corpus = text_load(FILE_PATH, TEXT_SIZE)
 
-    text = text_cleanup(text)
+    model = Word2Vec()
 
-    dict = text_to_dict(text)
+    model.train(corpus, 5)
